c7ea7869a7bb703ecfd156b86f2a866f4f38151f
simon jamain <jamain.simon@gmail.com>
2015-04-14 17:13:56 +0200

    - number of file reduced to keep only this I'm using

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # or-tools-binaries
 A mirror of google or-tool library which can also be found here https://app.box.com/s/gmgtnfqgl8ibrz5d1cm0
 
+This only support c++ linux 64 and mac osx 64 versions
+
 You can find more about google or-tools library through those links
 
 * https://developers.google.com/optimization/
--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@ mkdir bin;
 cd bin/;
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
         # ...
-	tar -xvzf ../lib/Google.OrTools.cc.Linux64.3853.tar.gz;
+	tar -xzf ../lib/Google.OrTools.cc.Linux64.3853.tar.gz;
 	mv or-tools.Linux64/ or-tools/;
 elif [[ "$OSTYPE" == "darwin"* ]]; then
         # Mac OSX
-	tar -xvzf ../lib/Google.OrTools.cc.MacOsX64.3853.tar.gz;
+	tar -xzf ../lib/Google.OrTools.cc.MacOsX64.3853.tar.gz;
 	mv or-tools.MacOsX64/ or-tools/;
 #elif [[ "$OSTYPE" == "cygwin" ]]; then
         # POSIX compatibility layer and Linux environment emulation for Windows
@@ -17,3 +17,4 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
 #elif [[ "$OSTYPE" == "freebsd"* ]]; then
         # ...
 fi
+cd ..;
