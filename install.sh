dabe921ea78ed0eb57ae2a26b1119cd14df7bc95
simon jamain <jamain.simon@gmail.com>
2015-04-14 17:01:41 +0200

    - gitignore added - uncompress script added, for linux 64 and OSX 64

---
+++ install.sh
@@ -0,0 +1,19 @@
+mkdir bin;
+cd bin/;
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        # ...
+	tar -xvzf ../lib/Google.OrTools.cc.Linux64.3853.tar.gz;
+	mv or-tools.Linux64/ or-tools/;
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+	tar -xvzf ../lib/Google.OrTools.cc.MacOsX64.3853.tar.gz;
+	mv or-tools.MacOsX64/ or-tools/;
+#elif [[ "$OSTYPE" == "cygwin" ]]; then
+        # POSIX compatibility layer and Linux environment emulation for Windows
+#elif [[ "$OSTYPE" == "msys" ]]; then
+        # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+#elif [[ "$OSTYPE" == "win32" ]]; then
+        # I'm not sure this can happen.
+#elif [[ "$OSTYPE" == "freebsd"* ]]; then
+        # ...
+fi
